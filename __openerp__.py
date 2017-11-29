a82eb19e5c85ec57b6eb6178d6e847b580056c08
jorge038 <34109677+jorge038@users.noreply.github.com>
2017-11-29 15:42:24 -0600

    Add files via upload

---
+++ __openerp__.py
@@ -0,0 +1,9 @@
+{
+
+    'name': 'hr_expense_maderma',
+    'description': 'Campos adicionales hr_expense',
+    'author': 'Jorge Maldonado',
+    'application': True,
+    'data' : ['hr_expense_maderma_view.xml'],
+    'depends':['base','hr_expense'],
+}
