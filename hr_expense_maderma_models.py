be78e246f68083caea30d6d09821c6b635fbafff
jorge038 <34109677+jorge038@users.noreply.github.com>
2017-11-29 15:40:49 -0600

    Create hr_expense_maderma_models.py

---
+++ hr_expense_maderma_models.py
@@ -0,0 +1,51 @@
+#-*- coding: utf-8 -*-
+import itertools
+from lxml import etree
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm, Warning, RedirectWarning
+from openerp.tools import float_compare
+import openerp.addons.decimal_precision as dp
+
+
+
+
+class hr_expense_expense(models.Model):                 #Creamos la clase hr_expense
+        _inherit = 'hr.expense.expense'                 #Decimos que sera extension del modeolo hr.expense.expense
+        @api.depends('line_ids')                                                #Reescribimos el metodo _amount que es de hr_expense class hr_expense_expense
+        def _amount(self):
+                        for expense in self:
+                                total = 0.0
+                                for line in expense.line_ids:
+                                        total += line.unit_amount * line.unit_quantity * (1 + line.product_id.supplier_taxes_id.amount)
+
+
+
+        state_ids = fields.Many2one(comodel_name='res.country.state',string='Ciudad')           #Creamos el campo state ids que tiene referencia de modelo res.country.state
+        partner_ids = fields.Many2many(comodel_name='res.partner',string='Clientes')
+        motivos  = fields.Selection([                                                           #Creamos el campo tipo seleccion motivo de gasto
+                ('viaje','Viaje'),
+                ('junta de vendedores','Junta de vendedores'),
+                ('congreso','Congreso'),
+                ('revisita','Revisita'),
+                ('evento','Evento con cliente'),
+                ('capacitacion','Capacitacion')
+                ],'',help='Motivo de gasto')
+        amount = fields.Float(compute='_amount',store=True)                     #volvemos a declarar el campo amount que es un campo calculado
+
+
+
+
+
+class hr_expense_line(models.Model):
+        _inherit = "hr.expense.line"
+
+        @api.depends('product_id','unit_amount')
+        def _tax_line(self):
+                        for line in self:
+
+                                tax = 0.0
+                                for l in line.product_id.supplier_taxes_id:
+                                        tax += l.amount * line.unit_amount
+                                line.tax_line=tax
+
+        tax_line=fields.Float(compute='_tax_line',store=True)           # declaramos el campo tax_line que almacenara el iva de el producto
